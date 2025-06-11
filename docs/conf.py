5815131036b339e319c4365aadb924e25d0d308d
Vandana Sharma <125654122+13vandana@users.noreply.github.com>
2025-06-11 23:19:24 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,7 @@
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+    "navigation_depth": 4,
+    "style_nav_header_background": "#2980B9",
+    "logo_url": "https://yourdomain.com",  # 👈 Your custom home URL
+}
